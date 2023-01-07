9013195393ab59f799c1da2d076529058e831ea8
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-07 18:27:52 +0530

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
   return (
     <div>
       <Heading heading="About"/>
-      <Paragraphs paragraph="I am Ayushman Garg, a Frontend Web Developer from Delhi"/>
+      <Paragraphs paragraph="Hi ,I am Ayushman Garg."/>
       <Paragraphs paragraph="
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ratione, expedita veritatis dolorem magni animi praesentium voluptatibus vero debitis possimus? Unde architecto ea, facere, officiis nesciunt dignissimos officia nemo optio voluptas consequuntur vitae in non laboriosam atque fugiat mollitia facilis magni maiores soluta, dolore nihil error. Perferendis nulla eveniet eum!"/> 
+      I was introduced to coding in my 11th class through Python. It was a great experience learning it for 2 years. Later I started learning Web development in my college first year. Till now I have worked with technologies like HTML, CSS, JavaScript, ReactJs, Bootstrap and Tailwind. I continue to improve by exploring other tech like NodeJs and Firebase. Apart from development I am also doing DSA in Python from Leetcode"/> 
       <TechSkills/>
     </div>
   )
