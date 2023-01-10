821d39ef3d2547d0492eda99662e5af8c0e434f8
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-10 13:36:38 +0530

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,12 +3,12 @@ import Certificates from '../components/Certificates/Certificates'
 import Heading from '../components/Heading/Heading'
 import Paragraphs from '../components/Paragraphs/Paragraphs'
 import TechSkills from '../components/TechSkills/TechSkills'
-
+import './About.css'
 const About = () => {
   return (
     <div>
       <Heading heading="About me"/>
-      <Paragraphs paragraph="Hi, I am Ayushman Garg."/>
+      <div className="para"><Paragraphs paragraph="Hi, I am Ayushman Garg. Please take a look around."/></div>
       <Certificates/>
       <TechSkills/>
     </div>
