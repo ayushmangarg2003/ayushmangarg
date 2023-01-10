7454d2401a977bf8452b886280cb209b595f5b3e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-10 13:00:54 +0530

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,14 +4,15 @@ import Paragraphs from '../components/Paragraphs/Paragraphs'
 import Socials from '../components/Socials/Socials'
 import "./Home.css"
 import ProfilePic from "../assets/ProfilePic.jpg"
+
 const Home = () => {
   return (
     <div className="container">
-      <Heading heading="Hi There!" />
+      <Heading heading="Hey There!" />
       <div className='home'>
         <img className='img' src={ProfilePic} alt="Ayushman Garg" />
         <div className="text">
-          <Paragraphs paragraph="I am Ayushman Garg, a Frontend Web Developer from Delhi"/>
+          <Paragraphs paragraph={`I am Ayushman Garg a frontend web developer. I was introduced to coding in my 11th class through Python. Later I started learning Web development in my first year and till now I have worked with technologies like HTML, CSS, JavaScript, ReactJs, Bootstrap and Tailwind. I usually create websites using ReactJS and other front-end tools. Apart from this I can even work on any other tech stack as I am a quick learner and can adapt to any new environment.`}/>
           <Socials/>
         </div>
       </div>
