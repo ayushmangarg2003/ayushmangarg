b3f2966d3fb078f631acac04d9d884aa196da41d
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-10 13:03:02 +0530

    Add files via upload

---
+++ src/components/Certificates/Certificates.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Certificates = () => {
+  return (
+    <div className="certificates">
+        <Heading/>
+        
+    </div>
+  )
+}
+
+export default Certificates
