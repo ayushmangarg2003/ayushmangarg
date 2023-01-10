7b574720da7bf1ebf489148c8b1fd8203c5bf14e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-10 12:29:00 +0530

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Heading from "../components/Heading/Heading"
 import Paragraphs from '../components/Paragraphs/Paragraphs'
 import Socials from '../components/Socials/Socials'
+import "./Home.css"
+import ProfilePic from "../assets/ProfilePic.jpg"
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="container">
       <Heading heading="Hi There!" />
-      <Paragraphs paragraph="I am Ayushman Garg, a Frontend Web Developer from Delhi"/>
-      <Socials/>
+      <div className='home'>
+        <img src={ProfilePic} alt="Ayushman Garg" />
+        <div className="text">
+          <Paragraphs paragraph="I am Ayushman Garg, a Frontend Web Developer from Delhi"/>
+          <Socials/>
+        </div>
+      </div>
     </div>
   )
 }
