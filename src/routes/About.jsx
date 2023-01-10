16acba80f219ee15a9fc564832f5ca590cf42e96
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-01-10 13:03:28 +0530

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Certificates from '../components/Certificates/Certificates'
 import Heading from '../components/Heading/Heading'
 import Paragraphs from '../components/Paragraphs/Paragraphs'
 import TechSkills from '../components/TechSkills/TechSkills'
@@ -7,9 +8,8 @@ const About = () => {
   return (
     <div>
       <Heading heading="About me"/>
-      <Paragraphs paragraph="Hi ,I am Ayushman Garg."/>
-      <Paragraphs paragraph="
-      I was introduced to coding in my 11th class through Python. It was a great experience learning it for 2 years. Later I started learning Web development in my college first year. Till now I have worked with technologies like HTML, CSS, JavaScript, ReactJs, Bootstrap and Tailwind. I continue to improve by exploring other tech like NodeJs and Firebase. Apart from development I am also doing DSA in Python from Leetcode"/> 
+      <Paragraphs paragraph="Hi, I am Ayushman Garg."/>
+      <Certificates/>
       <TechSkills/>
     </div>
   )
